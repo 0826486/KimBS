bb8280fc427335f97d745c94bc41e256d203cdf7
호정 <w2314@e-mirim.hs.kr>
2024-06-28 13:18:40 +0900

    굿즈 페이지 js를 바꿈

--- js/search.js
+++ js/search.js
@@ -1,13 +1,13 @@
 document.getElementById('search-button').addEventListener('click', function() {
     const searchInput = document.getElementById('search-input').value.toLowerCase().trim(); // 입력값을 소문자로 변환하고 공백을 제거합니다.
-    const articles = document.querySelectorAll('.img-container'); // 모든 article 요소를 선택합니다.
+    const images = document.querySelectorAll('.rize'); // 모든 이미지 요소를 선택합니다.
 
-    articles.forEach(article => {
-        const dataName = article.querySelector('a').getAttribute('data-name').toLowerCase(); // 각 article의 data-name 속성 값을 가져옵니다.
-        if (dataName.includes(searchInput)) {
-            article.style.display = 'block'; // 검색어가 포함된 경우 보이도록 설정합니다.
+    images.forEach(image => {
+        const altText = image.getAttribute('alt').toLowerCase(); // 각 이미지의 alt 속성 값을 가져옵니다.
+        if (altText.includes(searchInput)) {
+            image.closest('.ri').style.display = 'block'; // 검색어가 포함된 경우 이미지를 포함하는 부모 요소인 .ri를 보이도록 설정합니다.
         } else {
-            article.style.display = 'none'; // 검색어가 포함되지 않은 경우 숨깁니다.
+            image.closest('.ri').style.display = 'none'; // 검색어가 포함되지 않은 경우 해당 이미지를 포함하는 부모 요소를 숨깁니다.
         }
     });
 });
