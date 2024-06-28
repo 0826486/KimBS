aae849c37a24c8a80415dbd404d90b5a2ab4ecdd
호정 <w2314@e-mirim.hs.kr>
2024-06-28 18:09:51 +0900

    굿즈 페이지 js수정

--- js/search.js
+++ js/search.js
@@ -1,13 +1,14 @@
 document.getElementById('search-button').addEventListener('click', function() {
-    const searchInput = document.getElementById('search-input').value.toLowerCase().trim(); // 입력값을 소문자로 변환하고 공백을 제거합니다.
-    const images = document.querySelectorAll('.rize'); // 모든 이미지 요소를 선택합니다.
+    const searchInput = document.getElementById('search-input').value.toLowerCase().trim();
+    const items = document.querySelectorAll('.item'); // 모든 item 요소를 선택합니다.
 
-    images.forEach(image => {
-        const altText = image.getAttribute('alt').toLowerCase(); // 각 이미지의 alt 속성 값을 가져옵니다.
-        if (altText.includes(searchInput)) {
-            image.closest('.ri').style.display = 'block'; // 검색어가 포함된 경우 이미지를 포함하는 부모 요소인 .ri를 보이도록 설정합니다.
+    items.forEach(item => {
+        const dataName = item.getAttribute('data-name').toLowerCase();
+        const altText = item.querySelector('img').getAttribute('alt').toLowerCase();
+        if (dataName.includes(searchInput) || altText.includes(searchInput)) {
+            item.closest('.ri').style.display = 'block'; // 검색어가 포함된 경우 보이도록 설정합니다.
         } else {
-            image.closest('.ri').style.display = 'none'; // 검색어가 포함되지 않은 경우 해당 이미지를 포함하는 부모 요소를 숨깁니다.
+            item.closest('.ri').style.display = 'none'; // 검색어가 포함되지 않은 경우 숨깁니다.
         }
     });
 });
@@ -15,6 +16,6 @@ document.getElementById('search-button').addEventListener('click', function() {
 // 검색 입력창에서 엔터 키를 눌렀을 때도 검색이 작동하도록 이벤트 리스너 추가
 document.getElementById('search-input').addEventListener('keydown', function(event) {
     if (event.key === 'Enter') {
-        document.getElementById('search-button').click(); // Enter 키를 눌렀을 때 검색 버튼을 클릭하도록 설정합니다.
+        document.getElementById('search-button').click();
     }
 });
