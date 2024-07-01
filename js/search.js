b96c3350df93c7bddd99c74add843a39542d5966
호정 <w2314@e-mirim.hs.kr>
2024-07-02 01:43:10 +0900

    굿즈 js 살짝 변경

--- js/search.js
+++ js/search.js
@@ -1,14 +1,16 @@
 document.getElementById('search-button').addEventListener('click', function() {
     const searchInput = document.getElementById('search-input').value.toLowerCase().trim();
-    const items = document.querySelectorAll('.item'); // 모든 item 요소를 선택합니다.
+    const items = document.querySelectorAll('.ri'); // 모든 .ri 요소를 선택합니다.
 
     items.forEach(item => {
-        const dataName = item.getAttribute('data-name').toLowerCase();
-        const altText = item.querySelector('img').getAttribute('alt').toLowerCase();
+        const image = item.querySelector('img');
+        const dataName = item.querySelector('a').getAttribute('data-name').toLowerCase();
+        const altText = image.getAttribute('alt').toLowerCase();
+
         if (dataName.includes(searchInput) || altText.includes(searchInput)) {
-            item.closest('.ri').style.display = 'block'; // 검색어가 포함된 경우 보이도록 설정합니다.
+            item.style.display = 'block'; // 검색어가 포함된 경우 보이도록 설정합니다.
         } else {
-            item.closest('.ri').style.display = 'none'; // 검색어가 포함되지 않은 경우 숨깁니다.
+            item.style.display = 'none'; // 검색어가 포함되지 않은 경우 숨깁니다.
         }
     });
 });
@@ -16,6 +18,6 @@ document.getElementById('search-button').addEventListener('click', function() {
 // 검색 입력창에서 엔터 키를 눌렀을 때도 검색이 작동하도록 이벤트 리스너 추가
 document.getElementById('search-input').addEventListener('keydown', function(event) {
     if (event.key === 'Enter') {
-        document.getElementById('search-button').click();
+        document.getElementById('search-button').click(); // Enter 키를 눌렀을 때 검색 버튼을 클릭하도록 설정합니다.
     }
 });
